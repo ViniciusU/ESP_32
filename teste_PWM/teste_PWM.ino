a7d8bc8718fa9478fdf9de0ccdbd49ea5ee53636
rafaelheydt <rafaelheydt@gmail.com>
2023-08-16 17:47:53 -0300

    Update teste_PWM.ino

--- teste_PWM/teste_PWM.ino
+++ teste_PWM/teste_PWM.ino
@@ -5,13 +5,18 @@ const int freq = 10000;
 const int ledCanal = 0;
 const int resolucao = 8;
  
-void setup(){
+void setup()
+{
   // configura o PWM através dos parâmetros
   ledcSetup(ledCanal, freq, resolucao);
   
   // Anexar o canal ao GPIO
   ledcAttachPin(ledPino, ledCanal);
-  ledcWrite(ledCanal, 25);   
-   delay(15);
-  }
+  ledcWrite(ledCanal, 125);   
+  delay(15);
+  
+}
+void loop()
+{
+
 }
