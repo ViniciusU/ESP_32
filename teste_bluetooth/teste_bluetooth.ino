f6c8d3b1c9e387080c3f6b4776c69dcf75470f38
rafaelheydt <rafaelheydt@gmail.com>
2023-08-16 15:51:28 -0300

    Create teste_bluetooth.ino

---
+++ teste_bluetooth/teste_bluetooth.ino
@@ -0,0 +1,44 @@
+#include "BluetoothSerial.h"
+#include <LiquidCrystal.h>
+
+LiquidCrystal lcd(13, 12, 14, 27, 26, 25);
+BluetoothSerial SerialBT;
+char cmd;
+const int potPin = 34;
+int potValue;
+void setup() 
+{
+  //Serial.begin(115200);
+  delay(1000);
+  SerialBT.begin("ESP32");
+  pinMode(23,OUTPUT);
+  lcd.begin(16, 2);
+  // Clears The LCD Display
+  //lcd.clear();
+  lcd.print("Hello World!");
+  lcd.setCursor(1,1);
+  lcd.print("192");
+
+}
+
+void loop() {
+  potValue = analogRead(potPin);
+  SerialBT.println(potValue);
+
+  delay(500);
+  if(SerialBT.available())
+  {
+    cmd = SerialBT.read();
+
+  }
+  if(cmd =='1')
+  {
+    digitalWrite(23, HIGH);
+  }
+  if(cmd == '0')
+  {
+    digitalWrite(23, LOW);
+  }
+
+
+}
