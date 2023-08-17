58f9d86e7043901d857a6000cc1dd1b3c2232dd1
ViniciusU <vini.gama.15@gmail.com>
2023-08-17 16:42:12 -0300

    Webserver em tempo real

--- webserver/webserver.ino
+++ webserver/webserver.ino
@@ -1,12 +1,12 @@
 #include <WiFi.h>
 #include <WebServer.h>
 
-const char* ssid = "Tplink";
+const char* ssid = "vinicius";
 const char* password = "12345678";
-const int adcPin1 = 34;  // ADC pin for value 1
-const int adcPin2 = 35;  // ADC pin for value 2
-const int adcPin3 = 36;  // ADC pin for value 3
-const int adcPin4 = 39;  // ADC pin for value 4
+const int adcPin1 = 34;
+const int adcPin2 = 35;
+const int adcPin3 = 36;
+const int adcPin4 = 39;
 
 WebServer server(80);
 
@@ -25,7 +25,7 @@ void setup() {
   Serial.print("Endereço IP atribuído: ");
   Serial.println(WiFi.localIP());
   
-  server.on("/", handleRoot);
+  server.on("/", HTTP_GET, handleRoot);
   server.begin();
   
   Serial.println("Servidor Web iniciado");
@@ -48,16 +48,50 @@ void handleRoot() {
   html += ".container { max-width: 800px; margin: 50px auto; text-align: center; }";
   html += "h1 { color: #FFD700; }";
   html += "</style>";
+  html += "<script>";
+  html += "function updateValues() {";
+  html += "var xhr = new XMLHttpRequest();";
+  html += "xhr.onreadystatechange = function() {";
+  html += "if (xhr.readyState === 4 && xhr.status === 200) {";
+  html += "var data = JSON.parse(xhr.responseText);";
+  html += "document.getElementById('adcValue1').textContent = 'Valor ADC 1: ' + data.adcValue1;";
+  html += "document.getElementById('adcValue2').textContent = 'Valor ADC 2: ' + data.adcValue2;";
+  html += "document.getElementById('adcValue3').textContent = 'Valor ADC 3: ' + data.adcValue3;";
+  html += "document.getElementById('adcValue4').textContent = 'Valor ADC 4: ' + data.adcValue4;";
+  html += "}";
+  html += "};";
+  html += "xhr.open('GET', '/getADCValues', true);";
+  html += "xhr.send();";
+  html += "}";
+  html += "setInterval(updateValues, 2000);";
+  html += "</script>";
   html += "</head>";
   html += "<body>";
   html += "<div class=\"container\">";
   html += "<h1>Valores do ADC</h1>";
-  html += "<p>Valor ADC 1: " + String(adcValue1) + "</p>";
-  html += "<p>Valor ADC 2: " + String(adcValue2) + "</p>";
-  html += "<p>Valor ADC 3: " + String(adcValue3) + "</p>";
-  html += "<p>Valor ADC 4: " + String(adcValue4) + "</p>";
+  html += "<p id=\"adcValue1\">Valor ADC 1: Loading...</p>";
+  html += "<p id=\"adcValue2\">Valor ADC 2: Loading...</p>";
+  html += "<p id=\"adcValue3\">Valor ADC 3: Loading...</p>";
+  html += "<p id=\"adcValue4\">Valor ADC 4: Loading...</p>";
   html += "<p>Endereço IP atribuído: " + WiFi.localIP().toString() + "</p>";
   html += "</div>";
   html += "</body></html>";
+
   server.send(200, "text/html", html);
+}
+
+void handleGetADCValues() {
+  int adcValue1 = analogRead(adcPin1);
+  int adcValue2 = analogRead(adcPin2);
+  int adcValue3 = analogRead(adcPin3);
+  int adcValue4 = analogRead(adcPin4);
+
+  String json = "{";
+  json += "\"adcValue1\": " + String(adcValue1) + ",";
+  json += "\"adcValue2\": " + String(adcValue2) + ",";
+  json += "\"adcValue3\": " + String(adcValue3) + ",";
+  json += "\"adcValue4\": " + String(adcValue4);
+  json += "}";
+
+  server.send(200, "application/json", json);
 }
