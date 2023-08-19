68e2d17a475ba4a357f99cd0d4006f66779773dd
ViniciusU <vini.gama.15@gmail.com>
2023-08-19 16:33:41 -0300

    update appInv_02

--- AppInv_02Pots/AppInv_02Pots.ino
+++ AppInv_02Pots/AppInv_02Pots.ino
@@ -3,18 +3,34 @@
 BluetoothSerial SerialBT;
 char cmd;
 const int potPin = 34;
-int potValue;
+int ledRed = 12;
+int ledGreen = 13;
+int ledBlue = 14;
+int pwm_pin = 16;
 
-//POT 1
+int potValue;
 int sensorValue1 = 0;        // value read from the pot
 
+
+//configuração dos parâmetros do PWM
+const int freq = 10000;
+const int ledCanal = 0;
+const int resolucao = 8;
 
 
 void setup()
 {
   delay(1000);
   SerialBT.begin("ESP32");
-  pinMode(13,OUTPUT);
+  pinMode(ledRed,OUTPUT);
+  pinMode(ledGreen,OUTPUT);
+  pinMode(ledBlue,OUTPUT);
+   // configura o PWM através dos parâmetros
+  ledcSetup(ledCanal, freq, resolucao);
+  
+  // Anexar o canal ao GPIO
+  ledcAttachPin(ledPino, ledCanal);
+ 
   
 }
 
@@ -25,6 +41,27 @@ void loop()
 
   SerialBT.println(sensorValue1);
   SerialBT.println("/");
+
+  int switchState = SerialBT.read();
+
+   if (Serial.available())
+  {
+    valor = SerialBT.read();
+    if (valor == 245) digitalWrite(ledRed, HIGH);
+    if (valor == 246) digitalWrite(ledRed, LOW);
+    if (valor == 247) digitalWrite(ledGreen, HIGH);
+    if (valor == 248) digitalWrite(ledGreen, LOW);
+    if (valor == 249) digitalWrite(ledBlue, HIGH);
+    if (valor == 210) digitalWrite(ledBlue, LOW);
+    if ((valor >= 0) && (valor < 240))
+    {
+       ledcWrite(ledCanal, valor);
+    
+    
+    }
+  }
+
+
  
   delay(200);
 }
